3b32c5746a8a50b45d0c6064b87387bac1f41866
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-10-25 16:19:07 +0200

    Add test templates

--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here are our projects")
+    return render(request, 'projects.html')
 
 
 def project(request, pk):
-    return HttpResponse("SINGLE PROJECT" + " " + str(pk))
+    return render(request, 'single-project.html')
