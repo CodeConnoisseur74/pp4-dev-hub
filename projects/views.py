a62007ffed553f323c09620a05d94f51dacd5a74
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-10-29 17:51:26 +0100

    Setup init project forms

--- projects/views.py
+++ projects/views.py
@@ -1,31 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # noqa: F401
 from .models import Project
+#from .forms import ProjectForm
 
-projectsList = [
-    {
-        "id": "1",
-        "title": "Ecommerce Website",
-        "description": "Fully functional ecommerce website",
-    },
-    {
-        "id": "2",
-        "title": "Portfolio Website",
-        "description": "A Project from my portfolio",
-    },
-    {
-        "id": "3",
-        "title": "Social Network",
-        "description": "Awesome project in progress",
-    },
-]
+
+def createProject(request):
+    projects, search_query = () #searchProjects(request)
+    custom_range, projects = () #paginateProjects(request, projects, 6)
+
+    context = {
+        "projects": projects,
+        "search_query": search_query,
+        "custom_range": custom_range,
+    }
+    return render(request, "projects/projects.html", context)
 
 
 def projects(request):
     projects = Project.objects.all()
-    context = {'projects': projects}
+    context = {"projects": projects}
     return render(request, "projects/projects.html", context)
+
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, "projects/single-project.html", {'project': projectObj})
+    return render(request, "projects/single-project.html", {"project": projectObj})
