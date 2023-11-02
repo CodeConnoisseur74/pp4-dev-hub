4a8638f4eea9cf9f495616164ff86e891f506aa6
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-11-03 00:29:18 +0100

    fix images

--- devhub/settings.py
+++ devhub/settings.py
@@ -194,13 +194,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#MEDIA_URL = '/images/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / "static"
 ]
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
--- devhub/urls.py
+++ devhub/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf import settings
-#from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 #from django.contrib.auth import views as auth_views
 
@@ -27,8 +27,8 @@ urlpatterns = [
 ]
 
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 # 1 - User submits email for reset             //PasswordResetView.as_view()          //name="reset_password"
