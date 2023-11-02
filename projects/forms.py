872e52f5ef1cff5c7afa86e28b1bda414100572d
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-11-02 23:48:59 +0100

    fix CRUD in Project app

--- projects/forms.py
+++ projects/forms.py
@@ -7,15 +7,13 @@ from .models import Project #Review
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = '__all__'
-        # fields = [
-        #     "title",
-        #     "featured_image",
-        #     "description",
-        #     "demo_link",
-        #     "source_link",
-        #     "tags",
-        # ]
+        fields = [
+            "title",
+            "description",
+            "demo_link",
+            "source_link",
+            "tags",
+        ]
         #widgets = {
             #"tags": forms.CheckboxSelectMultiple(),
         #}
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 # from django.core import paginator
-from django.shortcuts import render #redirect
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 # from django.contrib.auth.decorators
@@ -37,11 +37,11 @@ def createProject(request):
     # profile = request.user.profile
     #form = ProjectForm()
 
-    #if request.method == "POST":
+    if request.method == 'POST':
         # newtags = request.POST.get("newtags").replace(",", " ").split()
-    form = ProjectForm(request.POST, request.FILES)
-        #if form.is_valid():
-            #form.save()
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
             # project = form.save(commit=False)
             # project.owner = profile
             # project.save()
@@ -50,41 +50,41 @@ def createProject(request):
             # for tag in newtags:
             # tag, created = Tag.objects.get_or_create(name=tag)
             # project.tags.add(tag)
-            # return redirect("account")
+            return redirect('projects')
 
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
 
 # @login_required(login_url="login")
-#def updateProject(request, pk):
+def updateProject(request, pk):
     # profile = request.user.profile
-    #project = Project.objects.get(id=pk)
-    #form = ProjectForm(instance=project)
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
 
-    #if request.method == "POST":
+    if request.method == 'POST':
         # newtags = request.POST.get("newtags").replace(",", " ").split()
 
-        #form = ProjectForm(request.POST, request.FILES, instance=project)
-        #if form.is_valid():
-            #form.save()
-            #return redirect("projects")
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
             # for tag in newtags:
             # tag, created = Tag.objects.get_or_create(name=tag)
             # project.tags.add(tag)
 
             # return redirect("account")
 
-    #context = {"form": form}
-    #return render(request, "projects/project_form.html", context)
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
 
 
 # @login_required(login_url="login")
-#def deleteProject(request, pk):
+def deleteProject(request, pk):
     # profile = request.user.profile
-    #project = profile.project_set.get(id=pk)
-    #if request.method == "POST":
-        #project.delete()
-        #return redirect("projects")
-    #context = {"object": project}
-    #return render(request, "delete_template.html", context)
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
