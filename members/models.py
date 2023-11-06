7a36a71e854762446e24fe80a698374bacae8ca5
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-11-06 17:06:53 +0100

    add projects pics and ownership

--- members/models.py
+++ members/models.py
@@ -8,8 +8,7 @@ import uuid
 
 
 class Profile(models.Model):
-    member = models.OneToOneField(
-        User, on_delete=models.CASCADE, null=True, blank=True)
+    member = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
@@ -17,39 +16,37 @@ class Profile(models.Model):
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(
-        null=True, blank=True, upload_to='profiles/', default="profiles/member-default.png")
+        null=True, blank=True, upload_to="profiles/", default="profiles/member-default.png"
+    )
     social_github = models.CharField(max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
     social_linkedin = models.CharField(max_length=200, blank=True, null=True)
     social_stackoverflow = models.CharField(max_length=200, blank=True, null=True)
     social_website = models.CharField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                        primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.username)
 
     class Meta:
-        ordering = ['created']
+        ordering = ["created"]
 
-#     @property
+    #     @property
     def imageURL(self):
         try:
             url = self.profile_image.url
         except:  # noqa: E722
-            url = ''
+            url = ""
         return url
 
 
 class Skill(models.Model):
-    owner = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                        primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.name)
