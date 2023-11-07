e8201cb28755e9983fac9cedd7f34e131cde7e04
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-11-07 14:51:11 +0100

    change username to membername

--- members/models.py
+++ members/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     member = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
-    username = models.CharField(max_length=200, blank=True, null=True)
+    membername = models.CharField(max_length=200, blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
@@ -27,7 +27,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.username)
+        return str(self.membername)
 
     class Meta:
         ordering = ["created"]
@@ -49,7 +49,7 @@ class Skill(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.username)
+        return str(self.membername)
 
 
 # class Message(models.Model):
