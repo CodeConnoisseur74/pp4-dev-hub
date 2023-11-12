52d0254f8d5f14e29cbae1840c60274d708bfdb1
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-11-12 21:57:41 +0100

    add login/logout authorization

--- members/views.py
+++ members/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
-
 # from django.dispatch.dispatcher import receiver
-# from django.shortcuts import render, redirect
-# from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 # from django.contrib.auth.decorators import login_required
 # from django.contrib import messages
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 # from django.urls import conf
 # from django.db.models import Q
 from .models import Profile
@@ -13,53 +11,55 @@ from .models import Profile
 # from .utils import searchProfiles, paginateProfiles
 
 
-# def loginUser(request):
+def loginMember(request):
 #     page = 'login'
 
 #     if request.user.is_authenticated:
 #         return redirect('profiles')
 
-#     if request.method == 'POST':
-#         username = request.POST['username'].lower()
-#         password = request.POST['password']
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-#         try:
-#             user = User.objects.get(username=username)
-#         except:
-#             messages.error(request, 'Username does not exist')
+        try:
+            member = User.objects.get(username=username)
+        except:  # noqa: E722
+            print('Username does not exist')
 
-#         user = authenticate(request, username=username, password=password)
+        member = authenticate(request, username=username, password=password)
 
-#         if user is not None:
-#             login(request, user)
-#             return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+        if member is not None:
+            login(request, member)
+        #     return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+            return redirect('profiles')
 
-#         else:
-#             messages.error(request, 'Username OR password is incorrect')
+        else:
+        #     messages.error(request, 'Username OR password is incorrect')
+            print('Username OR password is incorrect')
 
-#     return render(request, 'members/login_register.html')
-
-
-# def logoutUser(request):
-#     logout(request)
-#     messages.info(request, 'User was logged out!')
-#     return redirect('login')
+    return render(request, 'members/login_register.html')
 
 
-# def registerUser(request):
+def logoutMember(request):
+    logout(request)
+    # messages.info(request, 'Member was logged out!')
+    return redirect('login')
+
+
+# def registerMember(request):
 #     page = 'register'
 #     form = CustomUserCreationForm()
 
 #     if request.method == 'POST':
 #         form = CustomUserCreationForm(request.POST)
 #         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.username = user.username.lower()
-#             user.save()
+#             member = form.save(commit=False)
+#             member.username = member.username.lower()
+#             member.save()
 
 #             messages.success(request, 'User account was created!')
 
-#             login(request, user)
+#             login(request, member)
 #             return redirect('edit-account')
 
 #         else:
@@ -89,7 +89,7 @@ def memberProfile(request, pk):
 
 
 # @login_required(login_url='login')
-# def userAccount(request):
+# def memberAccount(request):
 #     profile = request.user.profile
 
 #     skills = profile.skill_set.all()
