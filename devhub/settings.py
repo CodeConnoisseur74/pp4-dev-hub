67be40083fa7d7b3fcc92dd08e6e39ba348d38b0
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-12-30 00:09:31 +0100

    fix environment variables

--- devhub/settings.py
+++ devhub/settings.py
@@ -179,7 +179,7 @@ AWS_S3_REGION_NAME = "eu-north-1"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
