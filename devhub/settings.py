ab2bdefd92de50886551f35a18c6d12f59081997
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-12-29 22:25:24 +0100

    update database url

--- devhub/settings.py
+++ devhub/settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = "devhub.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("devhub"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("devhub"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASS"),
+#         "HOST": os.environ.get("DB_HOST"),
+#         "PORT": "5432",
+#     }
+# }
 
 import dj_database_url
 
