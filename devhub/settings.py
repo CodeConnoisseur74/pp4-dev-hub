54d8bea2398dd31527011c1df8f04e271323469c
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-12-30 02:08:13 +0100

    remove unused settings not required

--- devhub/settings.py
+++ devhub/settings.py
@@ -159,17 +159,13 @@ STATIC_URL = "/static/"
 MEDIA_URL = "/media/"  # change this to /media/
 
 STATICFILES_STORAGE = "devhub.storages.StaticStorage"
-STATICFILES_LOCATION = "static"
 DEFAULT_FILE_STORAGE = "devhub.storages.MediaStorage"
-MEDIAFILES_LOCATION = "media"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # change to os.path.join(BASE_DIR, "static") ?
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # change this to /media
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
@@ -180,7 +176,6 @@ AWS_S3_REGION_NAME = "eu-north-1"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
